f4aa98f6a834fc988abbcaa751f2346669d92fe3
Siddharth Joisar <sidjoisar0807@gmail.com>
2023-12-12 01:46:05 +0530

    navbar

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <div>
     <App />
-  </React.StrictMode>
+    <ToastContainer />
+  </div>
 );
